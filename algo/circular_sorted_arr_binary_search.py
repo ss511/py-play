49f2fee3ac14d45b0f495ce0e2a7e5a9daf48162
ss511 <shashank.s4u@gmail.com>
2021-08-28 12:10:15 +0530

    [ALGO] Binary Search in a circularly sorted array.

---
+++ algo/circular_sorted_arr_binary_search.py
@@ -0,0 +1,31 @@
+def search(arr, key):
+    n = len(arr)
+    if n < 1:
+        return False
+
+    low, high = 0, n-1
+    # if low >= high:
+    #     return False
+
+    mid = n//2
+    if arr[mid] == key:
+        return True
+    if arr[low] <= arr[mid]:
+        if arr[low] <= key <= arr[mid]:
+            return search(arr[low:mid], key)
+        else:
+            return search(arr[mid+1:high+1], key)
+    else:
+        if arr[mid] <= key < arr[high]:
+            return search(arr[mid+1:high+1], key)
+        else:
+            return search(arr[low:mid], key)
+
+
+if __name__ == "__main__":
+    """
+    Algo to perform binary search in circularly sorted array.
+    """
+    input_arr = [5, 7, 8, 9, 1, 2, 3]
+    x = int(input())
+    print("Is X present in array: ", search(input_arr, x))
