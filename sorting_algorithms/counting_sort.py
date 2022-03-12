4891ae2bdd48af01b676bbeabab11f1e27036926
ss511 <shashank.s4u@gmail.com>
2022-03-12 20:52:27 +0530

    [Counting Sort] Other approach.

--- sorting_algorithms/counting_sort.py
+++ sorting_algorithms/counting_sort.py
@@ -24,6 +24,30 @@ def count_sort(arr):
     return output_arr
 
 
+def count_sort_v2(arr):
+    max_el = int(max(arr))
+    min_el = int(min(arr))
+    count = [0 for _ in range(max_el+1)]
+    output = [0 for _ in range(len(arr))]
+
+    for i in range(len(arr)):
+        count[arr[i]] += 1
+
+    k = 0
+    for i in range(len(count)):
+        j = count[i]
+        while j > 0:
+            output[k] = i
+            k += 1
+            j -= 1
+        i += 1
+
+    return output
+
+
 if __name__ == "__main__":
     print(count_sort([1, 0, 1, 3, 1, 3]))
     print(count_sort([1, 4, 1, 2, 7, 5, 2]))
+
+    print(count_sort_v2([1, 0, 1, 3, 1, 3]))
+    print(count_sort_v2([1, 4, 1, 2, 7, 5, 2]))
