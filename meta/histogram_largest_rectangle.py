c30118f00a2dca901014cf8158ba04b5e029ae51
ss511 <shashank.s4u@gmail.com>
2022-05-31 11:03:10 +0530

    [Meta] Largest Rectangular Area in a Histogram.

--- meta/histogram_largest_rectangle.py
+++ meta/histogram_largest_rectangle.py
@@ -1,5 +1,5 @@
 """
-Largest Rectangular Area in a Histogram
+Largest Rectangular Area in a Histogram.
 
 Find the largest rectangular area possible in a given histogram where the largest rectangle can be made of a number of
 contiguous bars. For simplicity, assume that all bars have same width and the width is 1 unit.
